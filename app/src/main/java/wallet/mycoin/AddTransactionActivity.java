ae3a6b060a4b94ebb9a08d195d8ff14821136cad
Arundas <arun.das@attinadsoftware.com>
2017-12-28 12:54:35 +0530

    comments removed

--- app/src/main/java/wallet/mycoin/AddTransactionActivity.java
+++ app/src/main/java/wallet/mycoin/AddTransactionActivity.java
@@ -40,7 +40,6 @@ public class AddTransactionActivity extends AppCompatActivity {
     EditText dateTxt, totalTxt,priceTxt,feesEtx;
     EditText volumeEtx, unitPriceEtx ;
     Button saveBtn;
-    //ImageButton dateImg;
     Spinner coinSpinner;
     boolean isUpdate=false;
 
@@ -138,7 +137,6 @@ public class AddTransactionActivity extends AppCompatActivity {
         priceTxt = findViewById(R.id.priceEtx);
         feesEtx = findViewById(R.id.feesEtx);
         totalTxt = findViewById(R.id.totalTxt);
-        //dateImg = findViewById(R.id.dateImg);
         coinSpinner = findViewById(R.id.coinSpinner);
         tranasctionType.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -175,7 +173,7 @@ public class AddTransactionActivity extends AppCompatActivity {
                 },
                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
                calendar.get(Calendar.DAY_OF_MONTH));
-                dialog.show();
+               dialog.show();
             }
         });
 
